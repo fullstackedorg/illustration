e4c405a90e1626b36fe7f1fe5480ed3468e27366
cplepage <lepage.charlesphilippe@gmail.com>
2024-12-30 23:08:51 -0500

    on resize

--- index.ts
+++ index.ts
@@ -68,3 +68,10 @@ function animate() {
     stats.update();
     renderer.render(scene, camera);
 }
+
+window.addEventListener("resize", onWindowResize, false);
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
