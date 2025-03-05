09f6be38e4eea054efd82c3146877f1849003d27
cplepage <lepage.charlesphilippe@gmail.com>
2025-03-05 13:39:00 -0500

    removed jszip

--- loaders/gltf.ts
+++ loaders/gltf.ts
@@ -1,29 +1,33 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import JSZip from "jszip";
 import {
     initShadowOnModel,
     loadDataFromLocalOrRemote,
     resizeAndCenterModel,
 } from "./utils";
 import { CONFIG } from "../src/config";
+import archive from "archive";
 
 const manager = new THREE.LoadingManager();
 const loader = new GLTFLoader(manager);
-const jsZip = new JSZip();
 
 export async function loadGLTF(filename: string) {
     const data = await loadDataFromLocalOrRemote(filename);
 
     let url: string;
     if (filename.endsWith(".zip")) {
-        const zip = await jsZip.loadAsync(data);
-        const bin = await zip.file("scene.bin").async("blob");
+        const zip = await archive.unzip(data);
+        
+        const binData = zip["scene.bin"].contents;
+        const bin = new Blob([binData], { type: "application/octet-stream" });
         manager.setURLModifier((url) => {
             if (url.endsWith("scene.bin")) return URL.createObjectURL(bin);
             return url;
         });
-        url = URL.createObjectURL(await zip.file("scene.gltf").async("blob"));
+
+        const gltfData= zip["scene.gltf"].contents;
+        const gltf = new Blob([gltfData], { type: "application/octet-stream" })
+        url = URL.createObjectURL(gltf);
     } else {
         url = URL.createObjectURL(new Blob([data]));
     }
